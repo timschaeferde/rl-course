c1b65fe69cb3565860a800a8da6817b3a7e058d9
Tim <tim.schaefer.de@gmail.com>
2021-04-26 17:04:41 +0200

    solution ex01 final

--- ex01-bandits/ex01-bandits.py
+++ ex01-bandits/ex01-bandits.py
@@ -51,7 +51,7 @@ def greedy(bandit, timesteps):
 
 def epsilon_greedy(bandit, timesteps):
     # epsilon greedy action selection (you can copy your code for greedy as a starting point)
-    eps = .5 # init epsilon
+    eps = .1 # init epsilon
 
     rewards = np.zeros(bandit.n_arms)
     n_plays = np.zeros(bandit.n_arms)
@@ -82,6 +82,8 @@ def epsilon_greedy(bandit, timesteps):
             arm = np.argmax(Q)  # select greedy action
         else:
             arm = random.choice(possible_arms) # select random action
+
+            # Answer to 2 d): Here we could choose the second best for some probability as an additional strategy
 
         reward = bandit.play_arm(arm)  # play selected action
         n_plays[arm] += 1
